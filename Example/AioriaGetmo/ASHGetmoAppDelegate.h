5c09068afb5a90ed36adbb874ecf14a7a2107563
Gustavo Tagliari <gustavo@aioria.com.br>
2015-11-11 19:57:37 -0200

    ajustando import

--- Example/AioriaGetmo/ASHGetmoAppDelegate.h
+++ Example/AioriaGetmo/ASHGetmoAppDelegate.h
@@ -6,9 +6,8 @@
 //  Copyright (c) 2015 Gustavo Tagliari. All rights reserved.
 //
 
-@import UIKit;
-
-#import "AioriaGetmo/SmartpushSDK/SmartpushSDK.h"
+#import <UIKit/UIKit.h>
+#import <AioriaGetmo/SmartpushSDK.h>
 
 @interface ASHGetmoAppDelegate : UIResponder <UIApplicationDelegate, SmartpushSDKDelegate>
 
